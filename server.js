06f98fb1b9e876e023a542ce10b02f047c58b0cb
BrianChu <BrianChu13321@gmail.com>
2016-08-06 01:13:20 -0700

    hello world on index.html

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ var port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \ Authorization');
   next();
-});
-
-app.get('/', function(req, res) {
-  res.send('Welcome to the home page!');
 });
 
 app.listen(port);
